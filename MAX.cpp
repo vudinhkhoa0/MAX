79431c8641dd36f0338679bf24c771299f8a0ac2
vudinhkhoa0 <126996254+vudinhkhoa0@users.noreply.github.com>
2023-03-25 00:53:49 +0700

    Add files via upload

---
+++ MAX.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
+int main()
+{
+    freopen("test049.inp","r",stdin);
+    freopen("test049.out","w",stdout);
+    int n;
+    cin >> n;
+    vector <int> a(n+1);
+    int ans = 0 ;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (i == 2) ans = abs(a[2] - a[1]);
+        else if (i > 2) ans = gcd(ans, abs(a[i] - a[1]));
+    }
+    cout << ans;
+}
